f7af21802c63228da87f7fa26d39614403964ffa
MaryamHsn <34409766+MaryamHsn@users.noreply.github.com>
2021-11-27 09:10:24 +0330

    default jsx

--- src/app.jsx
+++ src/app.jsx
@@ -37,6 +37,12 @@ export class Application extends React.Component {
         return (
             <Card>
                 <CardTitle>Mrt Log</CardTitle>
+                <CardBody>
+                    <Alert
+                        variant="info"
+                        title={cockpit.format(_("Running on $0"), this.state.hostname)}
+                    />
+                </CardBody>
             </Card>
         );
     }
