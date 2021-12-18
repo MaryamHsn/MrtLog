c3aa14e27eead7aca886efd41da936af60400c37
MRT1 <m.hosseinkhani71@gmail.cpm>
2021-12-18 14:22:29 +0330

    yy

--- src/app.jsx
+++ src/app.jsx
@@ -32,18 +32,17 @@ export class Application extends React.Component {
     }
 
     readTextFile = file => {
-     var rawFile = new XMLHttpRequest();
-     rawFile.open("GET", file, false);
-     rawFile.onreadystatechange = () => {
+        var rawFile = new XMLHttpRequest();
+        rawFile.open("GET", file, false);
+        rawFile.onreadystatechange = () => {
             if (rawFile.readyState === 4) {
                 if (rawFile.status === 200 || rawFile.status === 0) {
-                    var allText = rawFile.responseText;
                     this.setState({
                         text: allText
                     });
                 }
             }
-     };
+        };
         rawFile.send(null);
     };
 
