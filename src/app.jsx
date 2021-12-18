2d41572ce8d1b4776a01de3e596fa6e8d94d99c2
MRT1 <m.hosseinkhani71@gmail.cpm>
2021-12-18 14:15:30 +0330

    s

--- src/app.jsx
+++ src/app.jsx
@@ -23,39 +23,37 @@ export class Application extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-        text: ""
+            text: ""
         };
     }
 
     componentDidMount() {
         this.readTextFile(this.props.txt);
-	}
+    }
 
     readTextFile = file => {
      var rawFile = new XMLHttpRequest();
      rawFile.open("GET", file, false);
      rawFile.onreadystatechange = () => {
-         if (rawFile.readyState === 4) {
-             if (rawFile.status === 200 || rawFile.status == 0) {
-                 var allText = rawFile.responseText;
-                 this.setState({
-                     text: allText
-                 });
-             }
-         }
+            if (rawFile.readyState === 4) {
+                if (rawFile.status === 200 || rawFile.status === 0) {
+                    var allText = rawFile.responseText;
+                    this.setState({
+                        text: allText
+                    });
+                }
+            }
      };
-     rawFile.send(null);
-};
+        rawFile.send(null);
+    };
 
-render() {
-     return (
-         <div>
-             {this.state.text.split("\n").map((item, key) => {
-                 return <span key={key}>{item}<br /></span>;
-             })}
-         </div>
-		);
-	}
+    render() {
+        return (
+            <div>
+                {this.state.text.split("\n").map((item, key) => {
+                    return <span key={key}>{item}<br /></span>;
+                })}
+            </div>
+        );
+    }
 }
- 
-export default Application;
