811ea1a7a4e563bda9e8a2ae94918ed14bd3bba2
MRT1 <m.hosseinkhani71@gmail.cpm>
2021-12-18 13:16:14 +0330

    try

--- src/app.jsx
+++ src/app.jsx
@@ -17,31 +17,46 @@
  * along with Cockpit; If not, see <http://www.gnu.org/licenses/>.
  */
 
-import cockpit from 'cockpit';
-import React from 'react'; 
+import React from "react";
 
-const _ = cockpit.gettext;
+class Application extends React.Component {
+	constructor(props) {
+		super(props);
 
-export class Application extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+		this.state = {
+			text: ""
+		};
+	}
 
-    showFile = async (e) => {
-        e.preventDefault();
-        const reader = new FileReader();
-        reader.onload = async (e) => {
-            const text = (e.target.result);
-            console.log(text);
-            alert(text);
-        };
-        reader.readAsText(e.target.files[0]);
-    }
+	componentDidMount() {
+		this.readTextFile(this.props.txt);
+	}
 
-    render = () => {
-        return (<div>
-            <input type="file" onChange={(e) => this.showFile(e)} />
-        </div>
-        );
-    }
+	readTextFile = file => {
+		var rawFile = new XMLHttpRequest();
+		rawFile.open("GET", file, false);
+		rawFile.onreadystatechange = () => {
+			if (rawFile.readyState === 4) {
+				if (rawFile.status === 200 || rawFile.status == 0) {
+					var allText = rawFile.responseText;
+					this.setState({
+						text: allText
+					});
+				}
+			}
+		};
+		rawFile.send(null);
+	};
+
+	render() {
+		return (
+			<div>
+				{this.state.text.split("\n").map((item, key) => {
+					return <span key={key}>{item}<br /></span>;
+				})}
+			</div>
+		);
+	}
 }
+
+export default Application;
