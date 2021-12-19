042e63544bd23dc645791d5afd11adb3ddac100f
MRT1 <m.hosseinkhani71@gmail.cpm>
2021-12-19 11:16:53 +0330

    w

--- src/app.jsx
+++ src/app.jsx
@@ -20,7 +20,7 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-import "./styles.css";
+import "./app.scss";
 
 function App() {
     const [text, setText] = useState();
