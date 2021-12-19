beb6e49790a05dcc9ef386936f1176d8edb3bcc2
MRT1 <m.hosseinkhani71@gmail.cpm>
2021-12-19 11:48:50 +0330

    trygfv

--- src/app.jsx
+++ src/app.jsx
@@ -17,49 +17,15 @@
  * along with Cockpit; If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React, { useState } from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import File from './file.js';
 
-import "./app.scss";
-
-function App() {
-    const [text, setText] = useState();
-
-    let fileReader;
-
-    const onChange = e => {
-        const file = e.target.files;
-        fileReader = new FileReader();
-        fileReader.onloadend = handleFileRead;
-        fileReader.readAsText(file[0]);
-    };
-
-    const cleanContent = string => {
-        string = string.replace(/^\s*[\r\n]/gm, "");
-        const array = string.split(new RegExp(/[\r\n]/gm));
-        console.log(array);
-        array.splice(0, 3);
-        array.splice(-3);
-        return array.join("\n");
-    };
-
-    const handleFileRead = e => {
-        let content = fileReader.result;
-        // let text = deleteLines(content, 3);
-        content = cleanContent(content);
-        // … do something with the 'content' …
-        setText(content);
-    };
-    return (
-        <div className="App">
-            <div className="upload-btn-wrapper">
-                <button className="btn">Upload a file</button>
-                <input type="file" name="myfile" onChange={onChange} />
+export class Application extends React.Component {
+    render() {
+        return (
+            <div>
+                <File />
             </div>
-            {text && <pre>{text}</pre>}
-        </div>
-    );
+        );
+    }
 }
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
---
+++ src/file.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+function App() {
+    const [text, setText] = useState();
+
+
+    let fileReader;
+
+    const onChange = e => {
+        const file = e.target.files;
+        fileReader = new FileReader();
+        fileReader.onloadend = handleFileRead;
+        fileReader.readAsText(file[0]);
+    };
+
+
+    const cleanContent = string => {
+        string = string.replace(/^\s*[\r\n]/gm, "");
+        const array = string.split(new RegExp(/[\r\n]/gm));
+        console.log(array);
+        array.splice(0, 3);
+        array.splice(-3);
+        return array.join("\n");
+    };
+
+    const handleFileRead = e => {
+        let content = fileReader.result;
+        // let text = deleteLines(content, 3);
+        content = cleanContent(content);
+        // … do something with the 'content' …
+        setText(content);
+    };
+    return (
+        <div className="App">
+            <div className="upload-btn-wrapper">
+                <button className="btn">Upload a file</button>
+                <input type="file" name="myfile" onChange={onChange} />
+            </div>
+            {text && <pre>{text}</pre>}
+        </div>
+    );
+}
