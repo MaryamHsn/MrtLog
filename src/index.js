81cae0990355fae2898f3d1968d2104fb3805e17
MRT1 <m.hosseinkhani71@gmail.cpm>
2021-12-20 08:25:29 +0330

    aa

--- src/index.js
+++ src/index.js
@@ -22,8 +22,7 @@ import "./lib/patternfly/patternfly-4-cockpit.scss";
 import "core-js/stable";
 
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Application } from './app.jsx';
+import ReactDOM from 'react-dom'; 
 /*
  * PF4 overrides need to come after the JSX components imports because
  * these are importing CSS stylesheets that we are overriding
@@ -33,7 +32,59 @@ import { Application } from './app.jsx';
  */
 import "./lib/patternfly/patternfly-4-overrides.scss";
 import './app.scss';
+ 
+function App() {
+    const [text, setText] = useState();
 
-document.addEventListener("DOMContentLoaded", function () {
-    ReactDOM.render(React.createElement(Application, {}), document.getElementById('app'));
-});
+    const test = e => {
+        console.log(e.target.files);
+    };
+
+    let fileReader;
+
+    const onChange = e => {
+        let file = e.target.files;
+        fileReader = new FileReader();
+        fileReader.onloadend = handleFileRead;
+        fileReader.readAsText(file[0]);
+    };
+
+    const deleteLines = (string, n = 1) => {
+        console.log("remove lines");
+        return string.replace(new RegExp(`(?:.*?\n){${n - 1}}(?:.*?\n)`), "");
+    };
+
+    const cleanContent = string => {
+        string = string.replace(/^\s*[\r\n]/gm, "");
+        let array = string.split(new RegExp(/[\r\n]/gm));
+        console.log(array);
+        array.splice(0, 3);
+        array.splice(-3);
+        return array.join("\n");
+    };
+
+    const handleFileRead = e => {
+        let content = fileReader.result;
+        // let text = deleteLines(content, 3);
+        content = cleanContent(content);
+        // … do something with the 'content' …
+        setText(content);
+    };
+    return (
+        <div className="App">
+            <div class="upload-btn-wrapper">
+                <button class="btn">Upload a file</button>
+                <input type="file" name="myfile" onChange={onChange} />
+            </div>
+            {text && <pre>{text}</pre>}
+        </div>
+    );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
+
+
+//document.addEventListener("DOMContentLoaded", function () {
+//    ReactDOM.render(React.createElement(Application, {}), document.getElementById('app'));
+//});
